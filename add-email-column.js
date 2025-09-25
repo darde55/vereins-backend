881b5bdfe74cabe541091b5b8ddd834b1eef426e
darde55 <nick.bayer@gmx.de>
2025-09-25 17:04:36 +0200

    46dsasghjghj

--- add-email-column.js
+++ add-email-column.js
@@ -1,10 +1,2 @@
-const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('./termine.db');
-db.run('ALTER TABLE users ADD COLUMN email TEXT', (err) => {
-  if (err) {
-    console.error("Fehler oder Spalte existiert schon:", err.message);
-  } else {
-    console.log("E-Mail-Spalte hinzugefügt!");
-  }
-  db.close();
-});
+console.log("==MINIMAL TEST==: Backend wurde gestartet!");
+setInterval(() => {}, 10000); // hält Prozess am Leben
